b4a9a2be33db4887ad77670f24d71d2f0aa4a4bf
bruno.matos <bruno.matos@camtecnologia.com.br>
2022-07-03 23:09:11 -0300

    feat: :sparkles: Funcoes de Menu para o uso do sistema

---
+++ menu.py
@@ -0,0 +1,22 @@
+def menu():
+    print("Menu:\n")
+    print("1- Usuário")
+    print("2- Testar sistema de Baú")
+    print("3- Partida")
+    print("4- Região")
+    print("5- Forja de Cartas")
+    print("6- Forja de Decks")
+    print("7- Inventário(?)")
+    print("0- Sair")
+
+def menuRegiao():
+    print("Menu Região:\n")
+    print("1- Alterar Região")
+    print("2- Incrementar XP Região")
+    print("3- Avançar Nível Região")
+    print("0- Sair")
+
+def menuPartida():
+    print("Menu Partida:\n")
+    print("1- Jogar")
+    print("0- Sair")
