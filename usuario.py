af9acec73b17d82cc06658324a8758f47830439e
bruno.matos <bruno.matos@camtecnologia.com.br>
2022-07-03 23:08:29 -0300

    feat: :sparkles: Adicionando Regiao em Usuario

--- usuario.py
+++ usuario.py
@@ -1,13 +1,15 @@
 class Usuario: 
 
-    def __init__ (self, nome, qntPo, qntCoringa, exp, expRegiao, minhasCartas):
+    def __init__ (self, nome, qntPo, qntCoringa, exp, regiao, expRegiao, minhasCartas):
         self.nome = nome
         self.qntPo= qntPo
         self.qntCoringa = qntCoringa
         self.exp = exp
+        self.regiao = regiao
         self.expRegiao = expRegiao
         self.minhasCartas = minhasCartas
             
 
     def __str__ (self):
-        return "Usuário:\t{}\nPó de Carta:\t{}\nCoringa:\t{}\nXP da Conta:\t{}\nXP Região:\t{}\nMinhas Cartas:\t{}\n".format(self.nome, self.qntPo, self.qntCoringa, self.exp, self.expRegiao, self.minhasCartas)
+        print("")
+        return "Usuário:\t{}\nPó de Carta:\t{}\nCoringa:\t{}\nXP da Conta:\t{}\nRegião:\t{}\nXP Região:\t{}\nMinhas Cartas:\t{}\n".format(self.nome, self.qntPo, self.qntCoringa, self.exp, self.regiao, self.expRegiao, self.minhasCartas)
