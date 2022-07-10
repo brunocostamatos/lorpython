6c22c8606a01c15d4e01fbf64587fe71fa20c8a9
bruno.matos <bruno.matos@camtecnologia.com.br>
2022-07-10 00:40:52 -0300

    feat: :construction: criando os metodos de incrementar xp regiao e avancar nivel regiao e usar o metodo de bau

--- regiao.py
+++ regiao.py
@@ -1,8 +1,11 @@
+from bau import Bau
+
 class Regiao:
-    def __init__ (self, usuario, regiao, expRegiao):
+    def __init__ (self, usuario, regiao, expRegiao, nivelRegiao):
         self.usuario = usuario
         self.regiao = regiao
         self.expRegiao = expRegiao
+        self.nivelRegiao = nivelRegiao
     
     def listarRegioes(self): #precisa rever para quando a experiencia da atrelada a regiao e nao vai ser zerada sempre q mudar
         listaRegioes=['Aguas de Sentina', 'Bandópolis', 'Demacia', 'Freljord', 'Ilha das Sombras', 'Ionia', 'Noxus', 'Piltover e Zaun', 'Shurima', 'Targon']
@@ -32,13 +35,23 @@ class Regiao:
         print(self)
 
     def incrementarXPRegiao(self): #precisa rever para corrigir com o get e set expRegiao
-        self.expRegiao = self.expRegiao + 5
-        self.usuario.expRegiao = self.usuario.expRegiao + 5
-        print(self)
+        self.expRegiao += 10
+        self.usuario.expRegiao += 10
+        print("\nExperiência da Região {}".format(self.regiao) + ": {}".format(self.expRegiao) + "\n")
 
     def avancarNivelRegiao(self):  #precisa ver com o get e set expRegiao e nivelRegiao
-        pass
+        recompensa = Bau(self.usuario)
+        print(recompensa)
+        recompensa.sortearCartas([])
+        recompensa.verificarSorteio()
+        recompensa.tranformarSorteio()
+        self.expRegiao = 0
+        self.nivelRegiao += 1
+        self.usuario.expRegiao = 0
+        self.usuario.nivelRegiao += 1
+        print("\nBAÚ PARA O USUÁRIO\t{}\n".format(self.usuario.nome) + "Pó de Carta:\t{}\nCoringa:\t{}\nXP da Conta:\t{}\nNível da Conta:\t{}\nMinhas Cartas:\t{}\n".format(self.usuario.qntPo, self.usuario.qntCoringa, self.usuario.exp, self.usuario.nivel, self.usuario.minhasCartas))
+    
 
     def __str__ (self):
         print("")
-        return "REGIÃO ATUAL DO USUÁRIO\t{}\n".format(self.usuario.nome) + "Região:\t{}\nExperiência da Região:\t{}\n".format(self.regiao, self.expRegiao)
+        return "REGIÃO ATUAL DO USUÁRIO\t{}\n".format(self.usuario.nome) + "Região:\t{}\nNível da Região:\t{}\nExperiência da Região:\t{}\n".format(self.regiao, self.nivelRegiao, self.expRegiao)
