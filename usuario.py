36651a4befd89a4be0316acf3adc906c5cbe07e4
bruno.matos <bruno.matos@camtecnologia.com.br>
2022-07-10 00:41:58 -0300

    feat: :construction: adicionando nivel e nivel de regiao na classe usuario

--- usuario.py
+++ usuario.py
@@ -1,15 +1,17 @@
 class Usuario: 
 
-    def __init__ (self, nome, qntPo, qntCoringa, exp, regiao, expRegiao, minhasCartas):
+    def __init__ (self, nome, qntPo, qntCoringa, exp, nivel, regiao, expRegiao, nivelRegiao, minhasCartas):
         self.nome = nome
         self.qntPo= qntPo
         self.qntCoringa = qntCoringa
         self.exp = exp
+        self.nivel = nivel
         self.regiao = regiao
         self.expRegiao = expRegiao
+        self.nivelRegiao = nivelRegiao
         self.minhasCartas = minhasCartas
             
 
     def __str__ (self):
         print("")
-        return "Usuário:\t{}\nPó de Carta:\t{}\nCoringa:\t{}\nXP da Conta:\t{}\nRegião:\t{}\nXP Região:\t{}\nMinhas Cartas:\t{}\n".format(self.nome, self.qntPo, self.qntCoringa, self.exp, self.regiao, self.expRegiao, self.minhasCartas)
+        return "Usuário:\t{}\nPó de Carta:\t{}\nCoringa:\t{}\nXP da Conta:\t{}\nNível da Conta:\t{}\nRegião:\t{}\nNível Região:\t{}\nMinhas Cartas:\t{}\n".format(self.nome, self.qntPo, self.qntCoringa, self.exp, self.nivel, self.regiao, self.nivelRegiao, self.minhasCartas)
