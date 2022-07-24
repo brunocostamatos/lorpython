7d129d6657bfc0734c85d18f1c769bfbdbc3b340
bruno.matos <bruno.matos@camtecnologia.com.br>
2022-07-24 00:49:44 -0300

    refactor: :construction: Utilizando os metodos de get usuario, criando metodos de verificacao de arquivo e utilizacao de informaçoes salvas

--- persistencia.py
+++ persistencia.py
@@ -1,35 +1,66 @@
+import os.path
+import ast
+from usuario import Usuario
 
 class Persistencia:
-    def __init__ (self, nome, qntPo, qntCoringa, exp, nivel, regiao, expRegiao, nivelRegiao, minhasCartas):
-        self.nome = nome
-        self.qntPo= str(qntPo)
-        self.qntCoringa = str(qntCoringa)
-        self.exp = str(exp)
-        self.nivel = str(nivel)
-        self.regiao = regiao
-        self.expRegiao = str(expRegiao)
-        self.nivelRegiao = str(nivelRegiao)
-        self.minhasCartas = str(minhasCartas)
+    def __init__ (self, usuario):
+        self.usuario = usuario
+        self.nome = Usuario.getNome(usuario)
+        self.qntPo= str(Usuario.getQntPo(usuario))
+        self.qntCoringa = str(Usuario.getQntCoringa(usuario))
+        self.exp = str(Usuario.getExp(usuario))
+        self.nivel = str(Usuario.getNivel(usuario))
+        self.regiao = Usuario.getRegiao(usuario)
+        self.expRegiao = str(Usuario.getExpRegiao(usuario))
+        self.nivelRegiao = str(Usuario.getNivelRegiao(usuario))
+        self.minhasCartas = str(Usuario.getMinhasCartas(usuario))
+
+
 
     def ler(self):
-        f = open('infos.txt', 'r')
+        f = open('banco.txt', 'r')
         infos = f.read()
+        print()
         print(infos)
+        print()
         f.close()
 
-    def salvar(self,  nome, qntPo, qntCoringa, exp, nivel, regiao, expRegiao, nivelRegiao, minhasCartas):
-        f = open('infos.txt', 'w')
-        f.write(nome+"\n")
-        f.write(str(qntPo)+"\n")
-        f.write(str(qntCoringa)+"\n")
-        f.write(str(exp)+"\n")
-        f.write(str(nivel)+"\n")
-        f.write(regiao+"\n")
-        f.write(str(expRegiao)+"\n")
-        f.write(str(nivelRegiao)+"\n")
-        f.write(str(minhasCartas)+"\n")
-        print(self)
+    def salvar(self):
+        f = open('banco.txt', 'w')
+        f.write(self.nome+"\n")
+        f.write(self.qntPo+"\n")
+        f.write(self.qntCoringa+"\n")
+        f.write(self.exp+"\n")
+        f.write(self.nivel+"\n")
+        f.write(self.regiao+"\n")
+        f.write(self.expRegiao+"\n")
+        f.write(self.nivelRegiao+"\n")
+        f.write(self.minhasCartas)
         f.close()
+        print(self)
+
+    def verificarExistencia(): #metodo para verificar se o arquivo txt existe ou não, se nao existir irei fazer um usuario "novo"
+        existenciaArquivo = os.path.isfile('banco.txt')
+        return existenciaArquivo
+    
+    def recuperarInformacoes(self): #aqui entra o set atributos de usuario
+        f = open('banco.txt', 'r')
+        teste = []
+        for line in f:
+            teste.append(line.rstrip('\n'))
+        f.close()
+        self.usuario.nome = teste[0]
+        self.usuario.qntPo = int(teste[1])
+        self.usuario.qntCoringa = int(teste[2])
+        self.usuario.exp = int(teste[3])
+        self.usuario.nivel = int(teste[4])
+        self.usuario.regiao = teste[5]
+        self.usuario.expRegiao = int(teste[6])
+        self.usuario.nivelRegiao = int(teste[7])
+        convertendoLista = teste[8]
+        convertendoLista = ast.literal_eval(convertendoLista)
+        self.usuario.minhasCartas = convertendoLista
+    
     def __str__ (self):
         print("")
         return "Salvando\n"
